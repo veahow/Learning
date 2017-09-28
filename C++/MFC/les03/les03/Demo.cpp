85fd11f2dc5cd934201198116259168d3c43a225
Eahow Chen <veahow@gmail.com>
2017-09-28 10:42:15 +0800

    绘制圆形

---
+++ C++/MFC/les03/les03/Demo.cpp
@@ -0,0 +1,43 @@
+#include "Demo.h"
+
+BOOL CDemoApp::InitInstance()    // MFC入口函数
+{
+	CDemoWnd *pMainWnd = NULL;
+	pMainWnd = new CDemoWnd();
+	pMainWnd->Create(NULL, _T("My First MFC Program"));
+	
+	pMainWnd->ShowWindow(m_nCmdShow);
+	pMainWnd->UpdateWindow();
+	m_pMainWnd = pMainWnd;
+
+	return TRUE;
+}
+
+CDemoWnd::CDemoWnd()    // 构造函数
+{
+}
+
+CDemoWnd::~CDemoWnd()    // 析构函数
+{
+}
+
+BEGIN_MESSAGE_MAP(CDemoWnd, CFrameWnd)
+	ON_MESSAGE(WM_LBUTTONDOWN, OnLButtonDown)
+	ON_MESSAGE(WM_PAINT, OnPaint)
+END_MESSAGE_MAP()
+
+
+LRESULT CDemoWnd::OnLButtonDown(
+	WPARAM wParam, LPARAM lParam)
+{
+	//AfxMessageBox(_T("你按下了鼠标左键！"));
+	CClientDC dc(this);
+	dc.Ellipse(50, 50, 250, 250);
+	return 0;
+}
+
+LRESULT CDemoWnd::OnPaint(
+	WPARAM wParam, LPARAM lParam)
+{
+	return 0;
+}
